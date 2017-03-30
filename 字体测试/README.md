b29f58a86f5d212ba840179d5aedc1f03d1cd2cf
Irony <892768447@qq.com>
2017-03-31 01:50:13 +0800

    update

---
+++ 字体测试/README.md
@@ -0,0 +1,10 @@
+# 字体测试
+<br />
+
+### [Python3.4.4 or Python3.5][PyQt5]
+
+### 其中Roboto字体通过TTF编辑器修改了family,方便QFont加载
+
+# 截图
+<img src="ScreenShot/1.png" />
+<img src="ScreenShot/2.png" />
---
+++ 字体测试/提取字符/get.py
@@ -0,0 +1,36 @@
+# from bs4 import BeautifulSoup
+import re
+cheatsheet = open("cheatsheet.txt", "rb").read().decode()
+
+re_fa = re.compile("      fa(.*)")
+re_ch = re.compile("\[&amp;#x(.*?);\]")
+
+fas = re.findall(re_fa, cheatsheet)  # ['-500px\r', '-address-book\r']
+chs = re.findall(re_ch, cheatsheet)  # ['f26e', 'f2b9']
+assert len(fas) == len(chs)
+
+with open("result.txt", "w") as fp:
+    for i in range(len(fas)):
+        fp.write('fa{0} = "\\u{1}"'.format(
+            fas[i].replace("\r", "").replace("-", "_"),
+            chs[i])
+        )
+        fp.write("\n")
+print("ok")
+
+# bs = BeautifulSoup(cheatsheet)
+#
+# divs = bs.find_all("div", {"class":"col-print-4"})
+#
+# with open("result.txt","w") as fp:
+#     for div in divs:
+#         s = [repr(ss).replace("\\u","#") for ss in div.stripped_strings]
+#         print(s)
+#         if len(s)==3:
+#             fp.write('{1} = "{0}"'.format(*s))
+#         else:
+#             fp.write('{2} = "{1}"'.format(*s))
+#         fp.write("\n")
+# print("ok")
+# print(list(divs[0].stripped_strings))
+# >> ['4.4', '\uf26e', 'fa-500px', '[&#xf26e;]']
