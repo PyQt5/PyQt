21eed0286646c9c14029e6f12bcc1d13d9a40560
Irony <892768447@qq.com>
2018-05-06 01:42:03 +0800

    py转pyd

---
+++ C和C++扩展/py转pyd/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup, Extension
+
+from Cython.Distutils import build_ext
+
+
+setup(
+    cmdclass={'build_ext': build_ext},
+    ext_modules=[Extension("pydmod", sources=["pydmod.py"])]
+)
--- README.md
+++ README.md
@@ -37,6 +37,7 @@
  - [1.27 QRC资源文件使用](QRC资源文件使用/)
  - [1.28 C和C++扩展](C和C++扩展/)
      - [1.28.1 pyx和c++](C和C++扩展/pyx和c++/)
+     - [1.28.2 py转pyd](C和C++扩展/py转pyd/)
 
 
 ### [2.QGraphicsView练习](QGraphicsView练习/)
