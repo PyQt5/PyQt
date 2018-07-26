a91c1dde28a6b6693362864e73e08ef5ee0a2e95
Irony <892768447@qq.com>
2018-07-26 22:26:37 +0800

    test

--- QGraphicsView练习/QGraphicsItem/Item拖拽改变大小.py
+++ QGraphicsView练习/QGraphicsItem/Item拖拽改变大小.py
@@ -9,7 +9,7 @@ Created on 2018年7月26日
 @file: QGraphicsView练习.QGraphicsItem.Item移动
 @description: 
 """
-from PyQt5.QtCore import Qt, QLineF
+from PyQt5.QtCore import Qt, QLineF, QRectF
 from PyQt5.QtGui import QPainter, QColor, QPen
 from PyQt5.QtWidgets import QGraphicsRectItem, QGraphicsView, QGraphicsScene,\
     QStyle
@@ -26,6 +26,7 @@ class MoveableItem(QGraphicsRectItem):
 
     def __init__(self, *args, **kwargs):
         super(MoveableItem, self).__init__(*args, **kwargs)
+        self.setPos(0, 0)
         # 可移动,可选择,有焦点
         self.setFlags(self.ItemIsMovable |
                       self.ItemIsSelectable | self.ItemIsFocusable)
@@ -35,6 +36,7 @@ class MoveableItem(QGraphicsRectItem):
 
         # 是否在调整大小的状态
         self.isResizing = False
+        self.prePos = None
 
     def paint(self, painter, option, widget):
         super(MoveableItem, self).paint(painter, option, widget)
@@ -42,26 +44,78 @@ class MoveableItem(QGraphicsRectItem):
         if option.state & QStyle.State_Selected:
             rect = self.boundingRect()
             painter.setRenderHint(QPainter.Antialiasing, True)  # 抗锯齿
+            x, y, w, h = rect.x(), rect.y(), rect.width(), rect.height()
             if option.state & QStyle.State_HasFocus:  # 有焦点
-                painter.setPen(QPen(Qt.red, 2))  # 设置红色画笔
+                painter.setPen(QPen(Qt.red, 3))  # 设置红色画笔
                 painter.drawLines(  # 在左上、左下、右上、右下画8条小线段
-                    QLineF(rect.x(), rect.y(), rect.x()+10, rect.y()),  # 左上顶点向右
-                    QLineF(rect.x(), rect.y(), rect.x(), rect.y()+10),  # 左上顶点向下
-                    
-                    QLineF(rect.x(), rect.y()+rect.height(), rect.x()+10, rect.y()+rect.height()),  # 左下顶点向右
-                    QLineF(rect.x(), rect.y()+rect.height(), rect.x(), rect.y()+rect.height() - 10),  # 左下顶点向上
-                    
-                    QLineF(rect.width(), rect.y(), rect.width() - 10, rect.y()),  # 右上顶点向左
-                    QLineF(rect.width(), rect.y(), rect.width(), rect.y()+10),  # 右上顶点向下
-                    
-                    QLineF(rect.width(), rect.height(),
-                           rect.width() - 10, rect.height()),  # 右下顶点向左
-                    QLineF(rect.width(), rect.height(), rect.width(),
-                           rect.height() - 10)  # 右下顶点向上
+                    QLineF(x, y, x + 10, y),  # 左上顶点向右
+                    QLineF(x, y, x, y + 10),  # 左上顶点向下
+
+                    QLineF(x, y + h, x + 10, y + h),  # 左下顶点向右
+                    QLineF(x, y + h, x, y + h - 10),  # 左下顶点向上
+
+                    QLineF(x + w, y, x + w - 10, y),  # 右上顶点向左
+                    QLineF(x + w, y, x + w, y + 10),  # 右上顶点向下
+
+                    QLineF(x + w, y + h, x + w - 10, y + h),  # 右下顶点向左
+                    QLineF(x + w, y + h, x + w, y + h - 10)  # 右下顶点向上
                 )
 
     def hoverMoveEvent(self, event):
         super(MoveableItem, self).hoverMoveEvent(event)
+        # 鼠标悬停事件
+        cursor = self.isInResizeArea(event.pos())
+        if self.isResizing or (cursor and self.isSelected()):
+            # 正在调整中或者鼠标在可调整区域范围内并且是选中
+            self.setCursor(cursor)
+        else:
+            self.setCursor(Qt.ArrowCursor)
+
+    def mousePressEvent(self, event):
+        # 鼠标按下
+        super(MoveableItem, self).mousePressEvent(event)
+        if event.button() == Qt.LeftButton and self.isInResizeArea(event.pos()):
+            self.isResizing = True
+            self.prePos = event.pos()
+
+    def mouseReleaseEvent(self, event):
+        # 鼠标释放开
+        super(MoveableItem, self).mouseReleaseEvent(event)
+        if event.button() == Qt.LeftButton and self.isResizing:
+            self.isResizing = False
+            self.prePos =None
+
+    def mouseMoveEvent(self, event):
+        # 鼠标移动
+        if self.isResizing and self.prePos:
+            rect = self.boundingRect()
+            pos = event.pos()-self.prePos
+            w = pos.x()
+            h = pos.y()
+            x = -4 if w>0 else 4
+            y = -4 if h>0 else 4
+            print(x, y, -x, -y,pos)
+            self.setRect(rect.adjusted(x, y, -x, -y))
+            self.prepareGeometryChange()
+        else:
+            super(MoveableItem, self).mouseMoveEvent(event)
+
+    def isInResizeArea(self, pos):
+        # 检测判断鼠标所在位置是否为四个顶点的范围内
+        rect = self.boundingRect()
+        x, y, w, h = rect.x(), rect.y(), rect.width(), rect.height()
+
+        lx = pos.x() < x + 10
+        rx = pos.x() > x + w - 10
+        ty = pos.y() < y + 10
+        by = pos.y() > y + h - 10
+        # 左上角和右下角
+        if (lx and ty) or (rx and by):
+            return Qt.SizeFDiagCursor
+        # 右上角和左下角
+        if (rx and ty) or (lx and by):
+            return Qt.SizeBDiagCursor
+        return 0
 
 
 class ImageWidget(QGraphicsView):
