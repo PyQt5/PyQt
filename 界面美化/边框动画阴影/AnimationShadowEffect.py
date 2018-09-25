2001d9ad7554f45e5b2b64b22d9eae26c513546e
Irony <892768447@qq.com>
2018-09-25 23:31:08 +0800

    边框动画阴影

--- 界面美化/边框动画阴影/AnimationShadowEffect.py
+++ 界面美化/边框动画阴影/AnimationShadowEffect.py
@@ -9,9 +9,43 @@ Created on 2018年9月25日
 @file: AnimationShadowEffect
 @description: 边框动画阴影动画
 """
+from PyQt5.QtCore import QPropertyAnimation, pyqtProperty
+from PyQt5.QtWidgets import QGraphicsDropShadowEffect
+
 
 __Author__ = """By: Irony
 QQ: 892768447
 Email: 892768447@qq.com"""
 __Copyright__ = 'Copyright (c) 2018 Irony'
 __Version__ = 1.0
+
+
+class AnimationShadowEffect(QGraphicsDropShadowEffect):
+
+    def __init__(self, color, *args, **kwargs):
+        super(AnimationShadowEffect, self).__init__(*args, **kwargs)
+        self.setColor(color)
+        self.setOffset(0, 0)
+        self.setBlurRadius(0)
+        self._radius = 0
+        self.animation = QPropertyAnimation(self)
+        self.animation.setTargetObject(self)
+        self.animation.setDuration(2000)  # 一次循环时间
+        self.animation.setLoopCount(-1)  # 永久循环
+        self.animation.setPropertyName(b'radius')
+        # 插入线行值
+        self.animation.setKeyValueAt(0, 1)
+        self.animation.setKeyValueAt(0.5, 30)
+        self.animation.setKeyValueAt(1, 1)
+
+    def start(self):
+        self.animation.start()
+
+    @pyqtProperty(int)
+    def radius(self):
+        return self._radius
+
+    @radius.setter
+    def radius(self, r):
+        self._radius = r
+        self.setBlurRadius(r)
