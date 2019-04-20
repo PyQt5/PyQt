009e45ad0f8499d1fa5696cd8f65be9e75550959
weike32 <394967319@qq.com>
2019-04-20 11:32:46 +0800

    add treeWidget Demo

--- QTreeWidget/README.md
+++ QTreeWidget/README.md
@@ -2,10 +2,18 @@
 
 - 目录
   - [通过json数据生成树形结构](#1、通过json数据生成树形结构)
+  - [点击父节点全选/取消全选子节点](#2、点击父节点全选/取消全选子节点)
 
 ## 1、通过json数据生成树形结构
 [运行 ParsingJson.py](ParsingJson.py)
 
 解析每一层json数据中的list
 
+
 ![ParsingJson](ScreenShot/ParsingJson.png)
+## 2、点击父节点全选/取消全选子节点
+[运行 testTreeWidget.py](testTreeWidget.py)
+
+点击父节点全选/取消全选子节点
+
+![testTreeWidget](allSelect.png)
---
+++ QTreeWidget/testTreeWidget.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+Created on 2017年4月20日
+@author: weike32
+@site: https://pyqt5.com , https://github.com/weike32
+@email: 394967319@qq.com
+@file: CopyContent
+@description:
+'''
+import sys
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QDialog, QApplication
+from QTreeWidget.testTree import Ui_Form
+
+class graphAnalysis(QDialog,Ui_Form):
+    def __init__(self):
+        super(graphAnalysis, self).__init__()
+        self.setupUi(self)
+        self.treeWidget.itemChanged.connect(self.handleChanged)
+    def handleChanged(self,item,column):
+        count = item.childCount()
+        if item.checkState(column) == Qt.Checked:
+            for index in range(count):
+                item.child(index).setCheckState(0,Qt.Checked)
+        if item.checkState(column) == Qt.Unchecked:
+            for index in range(count):
+                item.child(index).setCheckState(0,Qt.Unchecked)
+
+
+if __name__ =="__main__":
+    app = QApplication(sys.argv)
+    w = graphAnalysis()
+    w.show()
+    sys.exit(app.exec_())
