4de63c3d4159774b513bd4824922544fd9d6ae27
weike32 <394967319@qq.com>
2019-05-21 18:09:29 +0800

    add treeWidget Demo

---
+++ PyQtGraph/Data/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: wxj
+@license: (C) Hefei tongzhi electromechanical control technology co.LTD
+@contact: 
+@software: garner
+@file: __init__.py.py
+@time: 2019/5/21 18:07
+@desc:
+'''
