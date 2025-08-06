24fa2e174b7129e28e8f64eae492aef5c8e9dda7
Irony <892768447@qq.com>
2025-08-06 14:32:50 +0800

    add filter model test

--- QTreeView/TestJsonModel.py
+++ QTreeView/TestJsonModel.py
@@ -12,24 +12,28 @@ Created on 2025/08/05
 
 import sys
 
-from Lib.qjsonmodel import QJsonModel
+from Lib.qjsonmodel import QJsonItem, QJsonModel
 
 try:
-    from PyQt5.QtCore import QModelIndex, Qt
+    from PyQt5.QtCore import QModelIndex, QSortFilterProxyModel, Qt
     from PyQt5.QtWidgets import (
         QAction,
         QApplication,
-        QHBoxLayout,
+        QCheckBox,
+        QGridLayout,
+        QLineEdit,
         QPlainTextEdit,
         QTreeView,
         QWidget,
     )
 except ImportError:
-    from PySide2.QtCore import QModelIndex, Qt
+    from PySide2.QtCore import QModelIndex, QSortFilterProxyModel, Qt
     from PySide2.QtWidgets import (
         QAction,
         QApplication,
-        QHBoxLayout,
+        QCheckBox,
+        QGridLayout,
+        QLineEdit,
         QPlainTextEdit,
         QTreeView,
         QWidget,
@@ -40,23 +44,38 @@ class TestWindow(QWidget):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.resize(800, 600)
-        layout = QHBoxLayout(self)
+        layout = QGridLayout(self)
+        self.filterPath = QCheckBox("Path Filter?", self)
+        self.filterEdit = QLineEdit(self)
         self.treeView = QTreeView(self)
         self.widgetEdit = QPlainTextEdit(self)
-        layout.addWidget(self.treeView)
-        layout.addWidget(self.widgetEdit)
+        layout.addWidget(self.filterPath, 0, 0, 1, 1)
+        layout.addWidget(self.filterEdit, 0, 1, 1, 1)
+        layout.addWidget(self.treeView, 1, 0, 1, 2)
+        layout.addWidget(self.widgetEdit, 0, 2, 2, 1)
+
+        self.filterPath.toggled.connect(self.onFilterPathToggled)
+        self.filterEdit.setPlaceholderText("过滤条件")
+        self.filterEdit.textChanged.connect(self.onFilterTextChanged)
 
         self.model = QJsonModel(self)
         self.model.itemChanged.connect(self.onItemChanged)
         self.model.rowsRemoved.connect(self.onRowsRemoved)
-        self.treeView.setModel(self.model)
         self.treeView.clicked.connect(self.onItemChanged)
         action = QAction("Delete", self.treeView)
         action.triggered.connect(self.deleteItem)
         self.treeView.setContextMenuPolicy(Qt.ActionsContextMenu)
         self.treeView.addAction(action)
 
+        # setup model data
         self.setupModel()
+
+        # filter model
+        self.fmodel = QSortFilterProxyModel(self)
+        self.fmodel.setSourceModel(self.model)
+        self.fmodel.setRecursiveFilteringEnabled(True)
+        self.treeView.setModel(self.fmodel)
+        self.treeView.expandAll()
 
     def setupModel(self):
         data = {
@@ -126,6 +145,15 @@ class TestWindow(QWidget):
         print(
             f"row={item.row()}, col={item.column()}, text={item.text()}, value={str(item.data(Qt.EditRole))}, key=({keyInfo})\n\trowCount={item.rowCount()}\n\tpath={item.path}"
         )
+
+    def onFilterPathToggled(self, checked):
+        if checked:
+            self.fmodel.setFilterRole(QJsonItem.PathRole)
+        else:
+            self.fmodel.setFilterRole(Qt.DisplayRole)
+
+    def onFilterTextChanged(self, text):
+        self.fmodel.setFilterFixedString(text)
 
 
 if __name__ == "__main__":
